193de634a29a6e72536c97bb15870bffc83e6c40
Bradley Hill <bradley.hill@ubisoft.com>
2024-12-05 14:29:40 +0100

    Still working on Day One...total is too low

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # adventOfCode2024
-Advent of Code 2024 attempt
+
+
+Advent of Code 2024 attempt, starting late so playing catch up.
+But also just as an exrcise to try to keep getting better at coding.
--- day1/index.js
+++ day1/index.js
@@ -1,15 +1,57 @@
-import fs from "fs";
+import fs from 'fs';
 
-fs.readFile("numbers.txt", "utf8", (err, data) => {
-  if (err) {
-    console.error("Error reading file:", err);
-    return;
-  }
+fs.readFile('numbers.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err);
+        return;
+    }
 
-  const dataLines = data.split("\n");
-  const firstLine = dataLines[0];
-  const [batch1, batch2] = firstLine.split(/\s{2,}/);
-  const leftArray = batch1.split("").sort((a, b) => a - b);
-  const rightArray = batch2.split("").sort((a, b) => a - b);
-  //TODO: Compare each digit in the arrays after sorting to discover the differences between them and keeping track of the difference total.
+    const dataLines = data.split('\n');
+
+    // console.log(dataLines);
+
+    const parsedArrays = dataLines.map(parseLinetoArrays);
+    const totalDifference = parsedArrays.reduce((total, { leftArray, rightArray }) => {
+        return total + calculateDifferenceTotal(leftArray, rightArray);
+    }, 0);
+
+    console.log('totalDifference is :>> ', totalDifference);
 });
+const parseLinetoArrays = (dataLine) => {
+    const cleanedLine = dataLine.replace(/\r/g, '').trim();
+    // console.log('cleanedLine :>> ', cleanedLine);
+
+    const parts = cleanedLine.split(/\s{2,}/);
+    // console.log('parts :>> ', parts);
+
+    // if (parts.length < 2) {
+    //     console.error('Invalid line format', dataLine);
+    //     return { leftArray: [], rightArray: [] };
+    // }
+    const [batch1, batch2] = parts;
+    const leftArray = transformBatch(batch1);
+    const rightArray = transformBatch(batch2);
+    return { leftArray, rightArray };
+};
+
+const calculateDifferenceTotal = (leftArray, rightArray) => {
+  console.log('leftArray :>> ', leftArray);
+  console.log('rightArray:>> ', rightArray);
+    let differenceTotal = 0;
+
+    leftArray.forEach((value, index) => {
+        const difference = Math.abs(value - rightArray[index]);
+        differenceTotal += difference;
+    });
+    return differenceTotal;
+};
+
+const transformBatch = (batch) => {
+    return batch
+        .replace(/\r/g, '')
+        .trim()
+        .split('')
+        .filter((char) => /\d/.test(char))
+        .map(Number)
+        .sort((a, b) => a - b);
+};
